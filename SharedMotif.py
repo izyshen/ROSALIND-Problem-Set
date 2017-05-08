df8d2227cdda892a287daab74e4b00334f9bc5b9
Iris Shen <ishen.zy@gmail.com>
2017-05-07 23:21:44 -0400

    method 2 using list of all substrings

--- SharedMotif.py
+++ SharedMotif.py
@@ -34,3 +34,31 @@ def common_substring(filename):
             l = len(substring)
             long.append(substring)
     return long
+
+
+####################################################
+
+
+def common_substring2(filename):
+    data = open(filename, 'r')
+    d = data.readlines()
+
+    ## produces a list of DNA strands
+    nuc = []
+    for line in d:
+        line = line.replace('\n', '')
+        if '>' not in line:
+            nuc.append(line)
+    print(nuc)
+    all_substrings = []
+    for i in range(len(nuc[0])):
+        for r in list(range(len(nuc[0])))[:0:-1]:
+            if nuc[0][i:r+1] not in all_substrings:
+                all_substrings.append(nuc[0][i:r+1])
+    print(all_substrings)
+    for other_nuc in nuc[1:]:
+        for substring in all_substrings:
+            if substring not in other_nuc:
+                all_substrings.remove(substring)
+    print(all_substrings)
+    return
